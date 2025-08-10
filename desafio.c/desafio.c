9f09c8ce9cff2e25345a618a7cf8754fa2cbce6d
marlinsonbr2020-collab <marlinsonbr2020@gmail.com>
2025-08-10 03:34:59 +0000

    criei o printf

---
+++ desafio.c/desafio.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int população = 10000;
+    float area = 1.7767;
+    float PIB = 6.53433;
+    int numero_de_pontos_turisticos = 10;
+
+    printf("carta n1 \n");
+
+    printf("qual o numero da população? ");
+    scanf("%d", &população);
+    printf("qual a area? ");
+    scanf("%f", &area);
+    printf("qual o PIB? ");
+    scanf("%f", &PIB);
+    printf("numero de pontos turistifcos? ");
+    scanf("%i", &numero_de_pontos_turisticos);
+
+    printf("carta n2 \n");
+
+    printf("qual o numero da população? ");
+    scanf("%d", &população);
+    printf("qual a area? ");
+    scanf("%f", &area);
+    printf("qual o PIB? ");
+    scanf("%f", &PIB);
+    printf("numero de pontos turistifcos? ");
+    scanf("%d", &numero_de_pontos_turisticos);
+
+
+
+
+}
